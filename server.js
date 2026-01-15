79fdca8edff3d76fbd1ed8125e33b668cedcf7a3
Perda <91690409+Listcz4638@users.noreply.github.com>
2026-01-16 00:18:43 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
