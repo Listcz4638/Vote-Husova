2912b005b93006951191a5bae0c62ba51e75b6d7
Perda <91690409+Listcz4638@users.noreply.github.com>
2026-03-04 14:42:15 +0100

    6.A

--- public/script.js
+++ public/script.js
@@ -5,10 +5,11 @@ let selectedCategory = localStorage.getItem("selectedCategory") || null;
 
 // ➤ Soutěžící (category: "1" = 1. stupeň, "2" = 2. stupeň)
 const participants = [
-  { name:"Jakub Svoboda, Hynek Dolejš - 9.A", img:"contestant1.jpg", video:"https://youtu.be/VIDEO1", category:"2" },
+  { name:"Antonín Fulka - 6.A", img:"contestant1.jpg", video:"https://youtube.com/shorts/Qv1WAwBhOz8", category:"2" },
+  { name:"Jakub Svoboda, Hynek Dolejš - 9.A", img:"contestant1.jpg", video:"", category:"2" },
   { name:"Elizaveta Bilonog - 9.B", img:"contestant1.jpg", video:"https://jumpshare.com/share/0Y8PCzsUATg918vQZNJm", category:"2" },
 
-  { name:"Bruno Kollmer - 1.B", img:"contestant7.jpg", video:"https://youtu.be/VIDEO2", category:"1" },
+  { name:"Bruno Kollmer - 1.B", img:"contestant7.jpg", video:"", category:"1" },
   { name:"Jakub Skibko (Matylda Vacková) - 2.B", img:"contestant8.jpg", video:"https://youtube.com/shorts/BmDKMQjjGvs", category:"1" },
 ];
 
@@ -134,4 +135,5 @@ window.addEventListener("DOMContentLoaded", () => {
 
   checkLogin();
 });
+
 
