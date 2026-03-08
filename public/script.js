4e23aacc5d5c271cd24ec3a1ff3865d25b836405
Perda <91690409+Listcz4638@users.noreply.github.com>
2026-03-08 18:56:01 +0100

    9.B update

--- public/script.js
+++ public/script.js
@@ -7,7 +7,7 @@ let selectedCategory = localStorage.getItem("selectedCategory") || null;
 const participants = [
   { name:"Antonín Fulka - 6.A", img:"contestant1.jpg", video:"https://youtube.com/shorts/Qv1WAwBhOz8", category:"2" },
   { name:"Jakub Svoboda, Hynek Dolejš - 9.A (video ještě nezasláno)", img:"contestant2.jpg", video:"", category:"2" },
-  { name:"Elizaveta Bilonog - 9.B", img:"contestant3.jpg", video:"https://files.fm/f/cqh2cyeyyh", category:"2" },
+  { name:"Elizaveta Bilonog - 9.B", img:"contestant3.jpg", video:"https://files.fm/f/surn24qk2s", category:"2" },
   { name:"Tereza Kohoutová - 8.A", img:"contestant3.jpg", video:"https://files.fm/f/7kp34z9q3j", category:"2" },
 
   { name:"Bruno Kollmer - 1.B", img:"contestant7.jpg", video:"https://youtube.com/shorts/Y8YbjGoD1O0", category:"1" },
@@ -136,6 +136,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   checkLogin();
 });
+
 
 
 
